32c4310ccfe54b82b46c5008f0ade5b52ce95c8f
vagrant <vagrant@precise64.(none)>
2013-11-21 08:22:40 +0000

    better btree_find_closest function (recursive)

--- btree/main.c
+++ btree/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_MAX 100
 
 struct s_btree_node
 {
@@ -12,65 +16,76 @@ struct s_btree_node
 
 struct s_btree_node *btree_find_closet(struct s_btree_node *root, char *value)
 {
-    struct s_btree_node *current_node;
-    current_node = root;
-    while (1)
+    if (strcmp(value, root->value) > 0)
     {
-        if (strcmp(value, current_node->value) > 0)
+        if (root->right == 0)
         {
-            if (current_node->right)
-            {
-                current_node = current_node->right;
-                continue;
-            }
-            return current_node;
+            return root;
         }
-        if (strcmp(value, current_node->value) < 0)
+        return btree_find_closet(root->right, value);
+    }
+    if (strcmp(value, root->value) < 0)
+    {
+        if (root->left == 0)
         {
-            if (current_node->left)
-            {
-                current_node = current_node->left;
-                continue;
-            }
-            return current_node;
+            return root;
         }
-        break;
+        return btree_find_closet(root->left, value);
     }
 
-    return current_node;
+    printf("FUCKING EQUALS\n");
+    exit(-1);
 }
 
-void btree_add(struct s_btree_node *node, char *value)
+void btree_add(struct s_btree_node *root, char *value)
 {
     struct s_btree_node *new_node;
-    struct s_btree_node *closest_node;
+    struct s_btree_node *cl_node;
+    char *value_copy;
+
     new_node = (struct s_btree_node *) malloc(sizeof (struct s_btree_node));
-    new_node->value = value;
-    closest_node = btree_find_closet(node, new_node->value);
-    if (strcmp(value, closest_node->value) > 0)
+    new_node->left = 0;
+    new_node->right = 0;
+    value_copy = malloc(sizeof(char) * strlen(value));
+    strcpy(value_copy, value);
+    new_node->value = value_copy;
+
+    cl_node = btree_find_closet(root, value);
+    if (strcmp(value, cl_node->value) > 0)
     {
-        closest_node->right = new_node;
+        printf("adding [%s] on the right of [%s]\n", new_node->value, cl_node->value);
+        cl_node->right = new_node;
     }
-    if (strcmp(value, closest_node->value) < 0)
+    else if (strcmp(value, cl_node->value) < 0)
     {
-        closest_node->left = new_node;
+        printf("adding [%s] on the left of [%s]\n", new_node->value, cl_node->value);
+        cl_node->left = new_node;
+    }
+    else {
+        printf("FUCKING EQUALS (2)[%s] [%s]\n", value, cl_node->value);
+        exit(-1);
     }
 }
+
 
 int main(int argc, char **argv)
 {
     struct s_btree_node *btree_root;
+    int nb_read;
+    char buffer[BUFFER_MAX];
+
     btree_root = (struct s_btree_node *) malloc(sizeof (struct s_btree_node));
     btree_root->left = 0;
     btree_root->right = 0;
-    btree_root->value = "b";
-    btree_add(btree_root, "a");
-    btree_add(btree_root, "c");
-    btree_add(btree_root, "d");
-    btree_add(btree_root, "f");
-    btree_add(btree_root, "e");
-    printf("%s %s %s %s %s %s\n", btree_root->value, btree_root->left->value, btree_root->right->value, btree_root->right->right->value,
-        btree_root->right->right->right->value, btree_root->right->right->right->left->value);
+    btree_root->value = "0";
+
+
+    while ((nb_read = read(0, buffer, BUFFER_MAX)) > 0)
+    {
+        buffer[nb_read] = '\0';
+        printf("buffer [%s]\n", buffer);
+        btree_add(btree_root, buffer);
+    }
 
     return 0;
 }
